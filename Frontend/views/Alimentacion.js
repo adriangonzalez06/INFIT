e37df86501df7031c0100a0356ff6d57eee9ef68
IBL\lidia.ingles.ext <lidia.ingles.ext@inetum.com>
2025-10-29 10:27:24 +0100

    Vista alimentación con scrolls y tarjetas

--- Frontend/views/Alimentacion.js
+++ Frontend/views/Alimentacion.js
@@ -1,18 +1,95 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+// Rutinas.js
+import React, { useState } from 'react';
+import {
+  View, Text, StyleSheet, TouchableOpacity, ScrollView, Modal, TextInput,
+  SafeAreaView,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Alimentacion() {
   const navigation = useNavigation();
 
+  const [recetas] = useState({
+    grupo1: [],
+    grupo2: [],
+    grupo3: [],
+    grupo4: [],
+    grupo5: [],
+    grupo6: [],
+  });
+
+  const renderGrupo = (titulo, recetasGrupo, grupoKey) => (
+    //Container group
+    <View style={styles.grupoContainer}>
+        //group title
+      <Text style={styles.grupoTitulo}>{titulo}</Text>
+        //routines row
+      <View style={styles.rutinasRow}>
+          //map displays a list of the items that are inside the function
+        {recetasGrupo.map((receta) => (
+          <TouchableOpacity
+            key={receta.id}
+            style={styles.recetaCard}
+            onPress={() => handleEntrarReceta(receta, grupoKey)}
+          >
+            <Text style={styles.recetaTexto}>{receta.nombre}</Text>
+          </TouchableOpacity>
+        ))}
+
+        <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+          <TouchableOpacity
+              style={styles.addCard}
+            >
+            <Ionicons name="add" size={32} color="#ef2b2d" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.rutinasCard} />
+          <TouchableOpacity style={styles.rutinasCard} />
+          <TouchableOpacity style={styles.rutinasCard} />
+
+          <TouchableOpacity
+              style={styles.seeMoreCard}
+            >
+            <Text style={{ color: '#000000ff', fontWeight: '600' }}>Ver más</Text>
+          </TouchableOpacity>
+
+        </ScrollView>
+
+      </View>
+    </View>
+  );
+
+  const handleEntrarReceta = (rutina, grupoKey) => {
+    navigation.navigate('PantallaRutina', {
+      rutina,
+      grupoKey,
+    });
+  };
+
   return (
     <View style={styles.container}>
+
+      //go back button
       <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
         <Ionicons name="arrow-back" size={24} color="#ef2b2d" />
       </TouchableOpacity>
-      <Text style={styles.title}>Tu alimentación recomendada</Text>
-      <Text style={styles.text}>Aquí verás tus comidas ideales para hoy.</Text>
+
+      //title
+      <Text style={styles.title}>Alimentación</Text>
+
+      //render groups
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <SafeAreaView>
+          {renderGrupo('Trending', recetas.grupo1, 'grupo1')}
+          {renderGrupo('Para ganar músculo', recetas.grupo2, 'grupo2')}
+          {renderGrupo('Para mantener la figura', recetas.grupo3, 'grupo3')}
+          {renderGrupo('Para mantener la figura', recetas.grupo4, 'grupo4')}
+          {renderGrupo('Para mantener la figura', recetas.grupo5, 'grupo5')}
+          {renderGrupo('Para mantener la figura', recetas.grupo6, 'grupo6')}
+        </SafeAreaView>
+      </ScrollView>
+
     </View>
   );
 }
@@ -42,5 +119,98 @@ const styles = StyleSheet.create({
     color: '#333',
     textAlign: 'center',
     marginBottom: 20,
+  },
+  scrollContent: {
+    paddingBottom: 40,
+  },
+  grupoContainer: {
+    marginBottom: 30,
+  },
+  grupoTitulo: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 10,
+    color: '#333',
+  },
+  rutinasRow: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 12,
+  },
+  rutinasCard: {
+    width: 120,
+    height: 100,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 12,
+  },
+  recetaTexto: {
+    fontSize: 14,
+    color: '#111114',
+    textAlign: 'center',
+  },
+  addCard: {
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#ef2b2d',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginRight: 12,
+  },
+    seeMoreCard: {
+    width: 120,
+    height: 100,
+    backgroundColor: '#ffffffff',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#494949ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 12,
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 12,
+    width: '80%',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 10,
+    color: '#111114',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  modalButton: {
+    backgroundColor: '#ef2b2d',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  modalButtonText: {
+    color: '#fff',
+    fontWeight: '600',
   },
 });
--- Frontend/views/ChatBot.js
+++ Frontend/views/ChatBot.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const NombreDelComponente = () => {
+
+ 
   return (
     <View style={styles.container}>
-      <Text>Componente de chatbot (sin hacer)</Text>
+    
+      <Text style={styles.title}>In-Fit-AI</Text>
+
+      <Text style={styles.botBubble}>This is a bot message</Text>
+      <Text style={styles.userBubble}>This is a user message</Text>
+
+      
+      <TextInput
+      style={styles.input}
+      placeholder="Pregunta algo..."
+      autoCapitalize="none"
+      />
+
     </View>
   );
 };
@@ -17,4 +31,46 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    color: '#111114',
+    backgroundColor: '#fff',
+    width: 250,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  botBubble: {
+    marginVertical: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    color: '#ffffffff',
+    backgroundColor: '#d46767ff',
+    alignSelf: 'center',
+  },
+  userBubble: {
+    marginVertical: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+    color: '#000000ff',
+    backgroundColor: '#ccc',
+    alignSelf: 'center',
+  }
+
 });
